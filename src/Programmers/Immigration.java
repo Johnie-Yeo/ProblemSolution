ccbd17590c121572e29e262924425b3bf08c0b3d
Johnie-Yeo <ducl7023@gmail.com>
2020-04-17 23:16:03 +0900

    2020 04 17

---
+++ src/Programmers/Immigration.java
@@ -0,0 +1,110 @@
+package Programmers;
+
+import Test.Test;
+
+import java.util.Arrays;
+
+public class Immigration {
+    public static void main(String[] args) {
+        new Immigration().test();
+    }
+    private void test(){
+        Test test = new Test();
+
+        int n = 6;
+        int[] times = {7, 10};
+        long expect = 28;
+        long result = solution(n, times);
+        test.test(result, expect);
+
+        n = 6;
+        times = new int[]{7, 10, 1000000000};
+        expect = 28;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 6;
+        times = new int[]{7, 10, 99999999, 1000000000};
+        expect = 28;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 6;
+        times = new int[]{7, 10, 20};
+        expect = 21;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 6;
+        times = new int[]{7, 10000000, 20000};
+        expect = 42;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 6;
+        times = new int[]{1000, 1002, 1003};
+        expect = 2006;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 8;
+        times = new int[]{7, 10, 20};
+        expect = 30;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 4;
+        times = new int[]{1,1,1,1};
+        expect = 1;
+        result = solution(n, times);
+        test.test(result, expect);
+
+        n = 1000000000;
+        times = new int[]{1000000000};
+        expect = 1000000000000000000L;
+        result = solution(n, times);
+    }
+    public long solution(int n, int[] times) {
+        long result = Long.MAX_VALUE;
+        long min = 0;
+        long max = getMaxTime(times, n);
+        while(min <= max){
+            long mid = (min + max) / 2;
+            long numOfPeoplePossibleToPassImmigrate
+                    = getNumOfPeoplePossibleToPassImmigrate(mid, times);
+            if(numOfPeoplePossibleToPassImmigrate >= n){
+                result = Math.min(result, mid);
+                if(max == mid){
+                    break;
+                }
+                max = mid;
+            }else{
+                if(min == mid){
+                    min++;
+                    continue;
+                }
+                min = mid;
+            }
+        }
+        return result;
+    }
+
+    private long getNumOfPeoplePossibleToPassImmigrate(long mid, int[] times) {
+        long people = 0;
+        for(int time : times){
+            long tmp = mid / time;
+            people += tmp;
+        }
+        return people;
+    }
+
+    private long getMaxTime(int[] times, int n) {
+        //가장 짧은 시간의 검사대에서 모든 사람이 검사받는게 최악
+        //어차피 여기서 병렬적으로 처리되서 계속 줄어들 것이므로
+        long min = Long.MAX_VALUE;
+        for(int time : times){
+            min = Math.min(min, time);
+        }
+        return min * n;
+    }
+}
---
+++ src/Programmers/PathToSchool.java
@@ -0,0 +1,88 @@
+package Programmers;
+
+import Test.Test;
+
+public class PathToSchool{
+    public static void main(String[] args) {
+        new PathToSchool().test();
+    }
+    private void test(){
+        Test test = new Test();
+
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2, 2}};
+        int expect = 4;
+        int result = solution(m, n, puddles);
+        test.test(result, expect);
+
+        m = 4;
+        n = 3;
+        puddles = new int[0][0];
+        expect = 10;
+        result = solution(m, n, puddles);
+        test.test(result, expect);
+
+        m = 3;
+        n = 4;
+        puddles = new int[][]{{2,1},{1,2}};
+        expect = 0;
+        result = solution(m, n, puddles);
+        test.test(result, expect);
+
+        m = 4;
+        n = 3;
+        puddles = new int[][]{{4,2},{3,3}};
+        expect = 0;
+        result = solution(m, n, puddles);
+        test.test(result, expect);
+
+        m = 5;
+        n = 3;
+        puddles = new int[][]{{2,1},{2,2}, {4,2}, {4,3}};
+        expect = 0;
+        result = solution(m, n, puddles);
+        test.test(result, expect);
+    }
+    public final int PUDDLE = -1;
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] map = setMap(m, n, puddles);
+        int MOD= 1000000007;
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(map[i][j] != PUDDLE){
+                    int left = map[i][j-1] == PUDDLE ? 0 : map[i][j-1];
+                    int up = map[i-1][j] == PUDDLE ? 0 : map[i-1][j];
+                    map[i][j] = (left + up) % MOD;
+                }
+            }
+        }
+
+        return map[n-1][m-1];
+    }
+
+    private int[][] setMap(int m, int n, int[][] puddles) {
+        int[][] map = new int[n][m];
+
+        for(int[] puddle : puddles){
+            int x = puddle[0] - 1;
+            int y = puddle[1] - 1;
+            map[y][x] = PUDDLE;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(map[i][0] == PUDDLE){
+                break;
+            }
+            map[i][0] = 1;
+        }
+        for(int i = 0; i < m; i++){
+            if(map[0][i] == PUDDLE){
+                break;
+            }
+            map[0][i] = 1;
+        }
+
+        return map;
+    }
+}
