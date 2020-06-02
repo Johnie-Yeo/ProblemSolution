a0498b541302ad20400c5a89d9968f83338cb519
Johnie-Yeo <ducl7023@gmail.com>
2020-06-03 01:25:35 +0900

    2020 06 02

--- src/BOJ/BabyShark.java
+++ src/BOJ/BabyShark.java
@@ -1,88 +1,224 @@
 package BOJ;
 
-import java.util.ArrayDeque;
+import Test.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BabyShark {
-	public static class Point{
+	public static void main(String[] args) {
+//		new Main().solve();
+		new BabyShark().test();
+	}
+
+	private void test() {
+		String input;
+		int expect;
+
+		input = "3\n" +
+				"0 0 0\n" +
+				"0 0 0\n" +
+				"0 9 0";
+		expect = 0;
+		testCase(input, expect);
+
+		input = "3\n" +
+				"0 0 1\n" +
+				"0 0 0\n" +
+				"0 9 0";
+		expect = 3;
+		testCase(input, expect);
+
+		input = "4\n" +
+				"4 3 2 1\n" +
+				"0 0 0 0\n" +
+				"0 0 9 0\n" +
+				"1 2 3 4";
+		expect = 14;
+		testCase(input, expect);
+
+		input = "6\n" +
+				"5 4 3 2 3 4\n" +
+				"4 3 2 3 4 5\n" +
+				"3 2 9 5 6 6\n" +
+				"2 1 2 3 4 5\n" +
+				"3 2 1 6 5 4\n" +
+				"6 6 6 6 6 6";
+		expect = 60;
+		testCase(input, expect);
+
+		input = "6\n" +
+				"6 0 6 0 6 1\n" +
+				"0 0 0 0 0 2\n" +
+				"2 3 4 5 6 6\n" +
+				"0 0 0 0 0 2\n" +
+				"0 2 0 0 0 0\n" +
+				"3 9 3 0 0 1";
+		expect = 48;
+		testCase(input, expect);
+
+		input = "6\n" +
+				"1 1 1 1 1 1\n" +
+				"2 2 6 2 2 3\n" +
+				"2 2 5 2 2 3\n" +
+				"2 2 2 4 6 3\n" +
+				"0 0 0 0 0 6\n" +
+				"0 0 0 0 0 9";
+		expect = 39;
+		testCase(input, expect);
+	}
+
+	private void testCase(String input, int expect) {
+		Test test = new Test();
+		String[] parsed = input.split("\n", 2);
+		int N = Integer.parseInt(parsed[0]);
+		int[][] map = InputParser.parseStringTo2DIntArray(parsed[1]);
+		int result = getTimeToFishing(N, map);
+		test.test(result, expect).printResult();
+	}
+
+	private void solve() {
+		Scanner kb = new Scanner(System.in);
+
+		int N = kb.nextInt();
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < N; j++) {
+				map[i][j] = kb.nextInt();
+			}
+		}
+		kb.close();
+		int result = getTimeToFishing(N, map);
+		System.out.println(result);
+	}
+
+	private class Point{
 		int x, y;
-		public Point(int x, int y) {
+		int dist;
+		public Point(int x, int y){
 			this.x = x;
 			this.y = y;
 		}
-		public Point addPoint(Point o) {
-			return new Point(x + o.x, y + o.y);
+
+		public Point(int x, int y, int dist){
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+		}
+		public void setDist(int dist){
+			this.dist = dist;
+		}
+
+		public void move(Point point) {
+			this.x = point.x;
+			this.y = point.y;
+		}
+
+		public Point clone(){
+			return new Point(this.x, this.y);
 		}
 	}
-	static Point []dir = { new Point(-1, 0), new Point(0, -1), new Point(0, 1), new Point(1, 0) };
-	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-		int N = kb.nextInt();
-		int [][]map = new int[N][N];
-		int curX = 0, curY = 0;
-		for(int i = 0; i < N; i++)
-			for(int j = 0; j < N; j++) {
-				map[i][j] = kb.nextInt();
-				if(map[i][j] == 9) {
-					curX = i;
-					curY = j;
-					map[i][j] = 0;
-				}	
-			}
-		kb.close();
-		System.out.println(getTimeOfGettingFish(map, curX, curY, N));		
-	}
-	private static int getTimeOfGettingFish(int[][] map, int curX, int curY, int N) {
+
+	private int getTimeToFishing(int n, int[][] map) {
 		int sharkSize = 2;
-		int fish = 0;
-		int step = 0;
-		int [][]check = new int [N][N];
-		Queue<Point> cur = new ArrayDeque<Point>();
-		Point p = new Point(curX, curY);
-		Point removeP = null;
-		cur.add(p);
-		check[p.x][p.y] = 1;
-		while(true) {
-			if(!cur.isEmpty()) {
-				p = cur.remove();
-				for(int i = 0; i < 4; i++) {
-					Point tmp = p.addPoint(dir[i]);
-					if(tmp.x < 0 || tmp.y < 0 || tmp.x >= N || tmp.y >= N)
-						continue;
-					if(check[tmp.x][tmp.y] == 0 && map[tmp.x][tmp.y] <= sharkSize) {
-						cur.add(tmp);
-						check[tmp.x][tmp.y] = check[p.x][p.y] + 1;
-						if(map[tmp.x][tmp.y] < sharkSize && map[tmp.x][tmp.y] > 0) {
-							if(removeP == null)
-								removeP = tmp;
-							else if(check[removeP.x][removeP.y] == check[tmp.x][tmp.y]) {
-								if(removeP.x > tmp.x || (removeP.x == tmp.x && removeP.y > tmp.y))
-									removeP = tmp;
-							}
-						}
-					}
-				}
+		Point sharkPoint = getSharkPoint(map, n);
+
+		Point point = getHuntableFishPoint(map, n, sharkPoint, sharkSize);
+
+		int time = 0;
+		int eat = 0;
+		while(point != null){
+			time += point.dist;
+			sharkPoint.move(point);
+			eat++;
+			if(eat == sharkSize){
+				eat = 0;
+				sharkSize++;
 			}
-			else {
-				if(removeP == null)
-					break;
-				else {
-					map[removeP.x][removeP.y] = 0;
-					fish++;
-					step += (check[removeP.x][removeP.y] - 1);
-					check = new int[N][N];
-					cur.removeAll(cur);
-					cur.add(removeP);
-					check[removeP.x][removeP.y] = 1;
-					if(fish == sharkSize) {
-						sharkSize++;
-						fish = 0;
+			map[point.x][point.y] = 0;
+			point = getHuntableFishPoint(map, n, sharkPoint, sharkSize);
+		}
+
+		return time;
+	}
+
+	private Point getHuntableFishPoint(int[][] map, int n, Point sharkPoint, int sharkSize) {
+		final int[] dirX = {-1, 0, 0, 1};
+		final int[] dirY = {0, -1, 1, 0};
+
+		Queue<Point> queue = new LinkedList<>();
+		Point start = sharkPoint.clone();
+		start.setDist(0);
+		queue.add(start);
+
+		boolean[][] check = new boolean[n][n];
+		check[start.x][start.y] = true;
+		Point result = null;
+
+		while(!queue.isEmpty()){
+			Point cur = queue.poll();
+			if(result != null && cur.dist >= result.dist){
+				break;
+			}
+
+			for(int d = 0; d < 4; d++){
+				int x = cur.x + dirX[d];
+				int y = cur.y + dirY[d];
+				int dist = cur.dist;
+
+				if(isOutOfRange(x, y, n) || check[x][y]){
+					continue;
+				}
+
+				check[x][y] = true;
+				Point next = new Point(x, y, dist+1);
+
+				if(map[x][y] > 0 && map[x][y] < sharkSize){
+					if(result == null){
+						result = next;
+					}else{
+						result = getResult(next, result);
 					}
-					removeP = null;
+				}else if(map[x][y] == 0 || map[x][y] == sharkSize){
+					queue.add(next);
 				}
 			}
 		}
-		return step;
+		return result;
+	}
+
+	private Point getResult(Point a, Point b) {
+		if(a.x != b.x){
+			if(a.x < b.x){
+				return a;
+			}else{
+				return b;
+			}
+		}else{
+			if(a.y < b.y){
+				return a;
+			}else{
+				return b;
+			}
+		}
+	}
+
+	private boolean isOutOfRange(int x, int y, int n) {
+		return (x < 0 || y < 0 || x >= n || y >= n);
+	}
+
+	private Point getSharkPoint(int[][] map, int n) {
+		final int SHARK = 9;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(map[i][j] == SHARK){
+					map[i][j] = 0;
+					return new Point(i, j);
+				}
+			}
+		}
+		return null;
 	}
 }
--- src/BOJ/TreeInvestment.java
+++ src/BOJ/TreeInvestment.java
@@ -1,139 +1,347 @@
 package BOJ;
 
+import Test.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class TreeInvestment {
-
-	public static class Tree{
-		int age;
-		public Tree(int age) {
-			this.age = age;
-		}
+	public static void main(String[] args) {
+//		new Main().solve();
+		new TreeInvestment().test();
 	}
-	public static void main(String[] args) throws IOException {
+
+	private void test() {
+		Test test = new Test();
+
+		String input;
+		int expect;
+
+		input = "1 1 1\n" +
+				"1\n" +
+				"1 1 1";
+		expect = 1;
+		testCase(test, input, expect);
+
+		input = "1 1 4\n" +
+				"1\n" +
+				"1 1 1";
+		expect = 0;
+		testCase(test, input, expect);
+
+		input = "5 2 1\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 2;
+		testCase(test, input, expect);
+
+		input = "5 2 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 15;
+		testCase(test, input, expect);
+
+		input = "5 2 3\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 13;
+		testCase(test, input, expect);
+
+		input = "5 2 4\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 13;
+		testCase(test, input, expect);
+
+		input = "5 2 5\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 13;
+		testCase(test, input, expect);
+
+		input = "5 2 6\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 85;
+		testCase(test, input, expect);
+
+		input = "2 2 5\n" +
+				"1 2\n" +
+				"2 1\n" +
+				"1 1 4\n" +
+				"2 2 4";
+		expect = 8;
+		testCase(test, input, expect);
+
+		input = "2 2 2\n" +
+				"1 2\n" +
+				"2 1\n" +
+				"1 1 1\n" +
+				"1 1 1\n" +
+				"1 1 1";
+		expect = 1;
+		testCase(test, input, expect);
+
+		input = "10 1 1000\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"100 100 100 100 100 100 100 100 100 100\n" +
+				"1 1 1";
+		expect = 5443;
+		testCase(test, input, expect);
+
+		input = "2 1 2\n" +
+				"0 100\n" +
+				"0 0\n" +
+				"1 2 5";
+		expect = 1;
+		testCase(test, input, expect);
+
+		input = "2 1 2\n" +
+				"0 100\n" +
+				"0 0\n" +
+				"1 2 5";
+		expect = 1;
+		testCase(test, input, expect);
+
+		input = "5 2 7\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 3 2 3 2\n" +
+				"2 1 3\n" +
+				"3 2 3";
+		expect = 71;
+		testCase(test, input, expect);
+	}
+
+	private void testCase(Test test, String input, int expect) {
+		String[] parsed = input.split("\n", 2);
+		int N = Integer.parseInt(parsed[0].split(" ")[0]);
+		int M = Integer.parseInt(parsed[0].split(" ")[1]);
+		int K = Integer.parseInt(parsed[0].split(" ")[2]);
+		parsed = parsed[1].split("\n", N+1);
+		int[][] A = new int[N][];
+		for(int i = 0; i < N; i++){
+			A[i] = InputParser.parseStringToIntArray(parsed[i]);
+		}
+		int[][] tree = InputParser.parseStringTo2DIntArray(parsed[N]);
+		int result = getNumberOfLiveTreeAfterKYears(N, M, K, A, tree);
+		test.test(result, expect).printResult();
+	}
+
+	private void solve() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String tmp = br.readLine();
-		StringTokenizer tk = new StringTokenizer(tmp);
-		int N = Integer.parseInt(tk.nextToken());
-		int M = Integer.parseInt(tk.nextToken());
-		int K = Integer.parseInt(tk.nextToken());
-		
-		int [][]A = new int[N+1][N+1];
-		int [][]nutrient = new int[N+1][N+1];
-		for(int i = 1; i <= N; i++) {
-			tmp = br.readLine();
-			tk = new StringTokenizer(tmp);
-			for(int j = 1; j <= N; j++) {
-				A[i][j] = Integer.parseInt(tk.nextToken());
-				nutrient[i][j] = 5;
+
+		try{
+			String line = br.readLine();
+			int N = Integer.parseInt(line.split(" ")[0]);
+			int M = Integer.parseInt(line.split(" ")[1]);
+			int K = Integer.parseInt(line.split(" ")[2]);
+
+			int[][] A = new int[N][N];
+
+			for(int i = 0; i < N; i++){
+				line = br.readLine();
+				String[] tmp = line.split(" ");
+				A[i] = Arrays.stream(tmp).mapToInt(e -> Integer.parseInt(e)).toArray();
 			}
-		}
-		
-		
-		@SuppressWarnings("unchecked")
-		ArrayList<Tree> [][]treeList = new ArrayList[N+1][N+1];
-		for(int i = 1; i <= N; i++)
-			for(int j = 1; j <= N; j++)
-				treeList[i][j] = new ArrayList<>();
-		
-		for(int i = 0; i < M; i++) {
-			tmp = br.readLine();
-			tk = new StringTokenizer(tmp);
-			int x = Integer.parseInt(tk.nextToken());
-			int y = Integer.parseInt(tk.nextToken());
-			int age = Integer.parseInt(tk.nextToken());
-			treeList[x][y].add(new Tree(age));
-		}
-		int count = M;
-	
-		for(int i = 0; i < K; i++) {
-			count += springSummer(treeList, nutrient, N);		
-			count +=autumn(treeList, N);
-			winter(nutrient, A, N);
-		}
-		System.out.println(count);
+			int[][] tree = new int[M][3];
+			for(int i = 0; i < M; i++){
+				line = br.readLine();
+				String[] tmp = line.split(" ");
+				tree[i] = Arrays.stream(tmp).mapToInt(e -> Integer.parseInt(e)).toArray();
+			}
+			br.close();
+			int result = getNumberOfLiveTreeAfterKYears(N, M, K, A, tree);
+			System.out.println(result);
+		}catch (IOException e){}
 	}
-	private static int autumn(ArrayList<Tree>[][] list, int n) {
-		
-		int count = 0;
-		int []dirX = {-1, 0, 1, 1, 1, 0, -1, -1};
-		int []dirY = {-1, -1, -1, 0, 1, 1, 1, 0};
-		
-		@SuppressWarnings("unchecked")
-		ArrayList<Tree> [][]tmpList = new ArrayList[n+1][n+1];
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++)
-				tmpList[i][j] = new ArrayList<>();
+
+
+	private class Point{
+		int x, y;
+		public Point(int x, int y){
+			this.x = x;
+			this.y = y;
 		}
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				int size = list[i][j].size();
-				for(int idx = size-1; idx >= 0; idx--) {
-					if(list[i][j].get(idx).age < 5)
-						break;
-					if(list[i][j].get(idx).age % 5 == 0) {
-						for(int d = 0; d < 8; d++) {
-							int x = i + dirX[d];
-							int y = j + dirY[d];
-							
-							if(x < 1 || y < 1 || x > n || y > n)
-								continue;
-							else {
-								tmpList[x][y].add(new Tree(1));
-								count++;
-							}
-						}
-					}
+
+		@Override
+		public int hashCode() {
+			return Arrays.hashCode(new Object[]{ this.x, this.y });
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Point p = (Point)obj;
+			return (this.x == p.x && this.y == p.y);
+		}
+	}
+	private int getNumberOfLiveTreeAfterKYears(int n, int m, int k, int[][] a, int[][] trees) {
+		HashMap<Point, ArrayList<Integer>> treeMap = initTreeMap(trees);
+		int[][] nourishment = initNourishment(n);
+
+		for(int year = 0; year < k; year++){
+			HashMap<Point, Integer> nourishmentFromDeadTrees = spring(treeMap, nourishment, n);
+			summer(nourishmentFromDeadTrees, nourishment);
+			autumn(treeMap, n);
+			winter(nourishment, a, n);
+		}
+
+		int result = getNumberOfLiveTree(treeMap, n);
+		return result;
+	}
+
+	private HashMap<Point, Integer> spring(HashMap<Point, ArrayList<Integer>> treeMap, int[][] nourishment, int n) {
+		HashMap<Point, Integer> nourishmentFromDeadTrees = new HashMap<>();
+
+		for(Point point : treeMap.keySet()){
+			ArrayList<Integer> ageList = treeMap.get(point);
+			ArrayList<Integer> grownList = new ArrayList<>();
+			int add = 0;
+
+			for(int age : ageList){
+				if(nourishment[point.x][point.y] >= age){
+					nourishment[point.x][point.y] -= age;
+					grownList.add(age+1);
+				}else{
+					add += (age/2);
 				}
 			}
+
+			treeMap.put(point, grownList);
+			nourishmentFromDeadTrees.put(point, add);
 		}
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++){
-				if(!tmpList[i][j].isEmpty())
-					list[i][j].addAll(0, tmpList[i][j]);
+
+		return nourishmentFromDeadTrees;
+	}
+
+	private void summer(HashMap<Point, Integer> nourishmentFromDeadTrees, int[][] nourishment) {
+		for(Point point : nourishmentFromDeadTrees.keySet()){
+			int x = point.x;
+			int y = point.y;
+			int add = nourishmentFromDeadTrees.get(point);
+			nourishment[x][y] += add;
+		}
+	}
+
+	private void autumn(HashMap<Point, ArrayList<Integer>> treeMap, int n) {
+		ArrayList<Point> pointList = new ArrayList<>(treeMap.keySet());
+		final int[]dirX = {-1, -1, -1, 0, 0, 1, 1, 1};
+		final int[]dirY = {-1, 0, 1, -1, 1, -1, 0, 1};
+		for(Point point : pointList){
+			ArrayList<Integer> ageList = treeMap.get(point);
+			int numberOfParent = getNumberOfParent(ageList);
+
+			for(int d = 0; d < 8; d++){
+				int x = point.x + dirX[d];
+				int y = point.y + dirY[d];
+				if(isOutOfRange(x, y, n)){
+					continue;
+				}
+				addTree(treeMap, x, y, numberOfParent);
 			}
 		}
-		
-		return count;
-		
 	}
-	private static int springSummer(ArrayList<Tree>[][] list, int[][] nut, int n) {
-		
+
+	private int getNumberOfParent(ArrayList<Integer> ageList) {
 		int count = 0;
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				for(int idx = 0; idx < list[i][j].size(); idx++) {
-					int age = list[i][j].get(idx).age;
-					if(age > nut[i][j]) {
-						//������� �ڿ����� �ѹ��� ����
-						//���� �ݸ�ŭ ��е� ���ϱ�
-						for(int k = idx; k < list[i][j].size(); k++) {
-							nut[i][j] += (list[i][j].get(k).age/2);
-							list[i][j].remove(k);
-							count--;
-							k--;
-						}
-					}
-					else {
-						nut[i][j] -= age;
-						list[i][j].get(idx).age++;
-					}
-				}
+		for(int age : ageList){
+			if(age % 5 == 0){
+				count++;
 			}
 		}
 		return count;
 	}
 
-	private static void winter(int[][] a, int[][] a2, int n) {
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++)
-				a[i][j] += a2[i][j];
+	private boolean isOutOfRange(int x, int y, int n) {
+		return (x < 0 || y < 0 || x >= n || y >= n);
+	}
+
+	private void addTree(HashMap<Point, ArrayList<Integer>> treeMap, int x, int y, int numberOfParent) {
+		Point point = new Point(x, y);
+		ArrayList<Integer> ageList = treeMap.get(point) == null ? new ArrayList<>() : treeMap.get(point);
+		for(int i = 0; i < numberOfParent; i++){
+			ageList.add(0, 1);
 		}
+		treeMap.put(point, ageList);
+	}
+
+	private void winter(int[][] nourishment, int[][] a, int n) {
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				nourishment[i][j] += a[i][j];
+			}
+		}
+	}
+
+	private int[][] initNourishment(int n) {
+		int[][] map = new int[n][n];
+		Arrays.stream(map).forEach(a -> Arrays.fill(a, 5));
+		return map;
+	}
+
+	private HashMap<Point, ArrayList<Integer>> initTreeMap(int[][] trees) {
+		HashMap<Point, ArrayList<Integer>> map = new HashMap<>();
+
+		for(int[] tree : trees){
+			int x = tree[0] - 1;
+			int y = tree[1] - 1;
+			int age = tree[2];
+			Point p = new Point(x, y);
+			ArrayList<Integer> list = new ArrayList<>();
+			list.add(age);
+			map.put(p, list);
+		}
+		return map;
+	}
+
+	private int getNumberOfLiveTree(HashMap<Point, ArrayList<Integer>> treeMap, int n) {
+		int result = 0;
+		for(Point p : treeMap.keySet()){
+			int size = treeMap.get(p).size();
+			result += size;
+		}
+		return result;
 	}
 }
