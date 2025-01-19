0d4ef62566a62f6870074a914232d3a24e4cb4ae
Johnie <ducl7023@gmail.com>
2025-01-19 23:04:58 +0900

    2025 01 19 - 1

--- src/Leetcode/medium/PartitionEqualSubsetSum.py
+++ src/Leetcode/medium/PartitionEqualSubsetSum.py
@@ -1,41 +1,21 @@
-# Failed Time Exceed!
-
-from typing import List, Set
+from typing import List
 import unittest
 
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        totalSum = sum(nums)
-
-        if totalSum % 2 == 1:
+        sumVal = sum(nums)
+        if sumVal % 2 == 1:
             return False
 
-        target = totalSum / 2
+        targetValue = sumVal / 2
         comb = set()
-        if nums:
-            comb.add(nums[0])
-        comb = self.getComb(nums, 1, comb, target)
+        for num in nums:
+            for e in list(comb):
+                comb.add(e+num)
+            comb.add(num)
 
-        return target in comb
-
-
-    def getComb(self, nums: List[int], idx: int, comb: Set[int], target: int) -> Set[int]:
-        if idx >= len(nums):
-            return comb
-
-        curTarget = list(comb)
-        curComb = set(curTarget)
-        result = set()
-        for num in curTarget:
-            if nums[idx] <= target:
-                curComb.add(nums[idx])
-            if num + nums[idx] <= target:
-                curComb.add(num + nums[idx])
-            result.update(self.getComb(nums, idx+1, curComb, target))
-        return result
-
-
+        return targetValue in comb
 
 
 class TestSolution(unittest.TestCase):
